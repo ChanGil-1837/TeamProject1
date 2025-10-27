839f719b281870c6c1089c55d2f6a06bc20cf4dd
MelonCorn <rhfoem456@gmail.com>
2025-10-27 15:20:37 +0900

    [feat] Weapon ,Projectile 공격 로직 추가
    Weapon
    - 투사체 pool
    - 발사 코루틴 (임시)
    
    Projectile
    - velocity 이동
    - Trigger 충돌
    - pool 반환
    
    ProjectSetting
    - Enemy 추가

--- Assets/02_Scripts/WeaponSystem/Weapon.cs
+++ Assets/02_Scripts/WeaponSystem/Weapon.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
+using Unity.IO.LowLevel.Unsafe;
 using UnityEngine;
 
 
@@ -17,49 +19,117 @@ public class Weapon : MonoBehaviour
 
     private WeaponType type;
 
-    private float damage;
+    [Header("공격력")]
+    [SerializeField] private float damage;
 
-    private float baseInterval;
-    private float currentInterval;
+    [Header("공격 주기")]
+    [SerializeField] private float baseInterval;
+    private Coroutine attackCoroutine;
+    private WaitForSeconds attackInterval;
+    private WaitUntil fireState;
 
-    private int level;
+    [Header("레벨")]
+    [SerializeField] private int level;
 
-    private Projectile projectilePrefab;
-    private int projectileCount;
+    [Header("투사체")]
+    [SerializeField] private Projectile projectilePrefab;
+    [SerializeField] private int projectileCount;
 
-    private int poolSize;
+    [Header("풀")]
+    [SerializeField] private int poolSize;
     private Queue<Projectile> projectilePool = new();
 
+    [SerializeField] Transform target;
+    private bool canFire;
 
-    void InitializePool()
+    private void Awake()
     {
-        // 투사체 풀 초기화
+        Init();
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            canFire = !canFire;
+        }
+    }
+
+    void Init()
+    {
+        fireState = new WaitUntil(() => canFire);
+        attackInterval = new WaitForSeconds(baseInterval);
+
+        attackCoroutine = StartCoroutine(Attack());    
+
+        for (int i = 0; i < poolSize; i++)
+        {
+            Projectile projectile = NewProjectile();
+        }
+    }
+
+    #region 풀링
+    // 새로운 투사체 생성
     private Projectile NewProjectile()
     {
-        // 새로운 투사체 생성
-        return null;
+        Projectile newProjectile = Instantiate(projectilePrefab, transform);
+        newProjectile.Init(this);
+        newProjectile.gameObject.SetActive(false);
+        projectilePool.Enqueue(newProjectile);
+
+        return newProjectile;
     }
-    private Projectile GetBulletFromPool()
+
+    // 풀에서 투사체 사용
+    private Projectile GetFromPool()
     {
-        // 풀에서 투사체 꺼냄
-        return null;
+        Projectile projectile;
+
+        // 남는 투사체 없음
+        if (projectilePool.Count <= 0)
+        {
+            projectile = NewProjectile();
+        }
+
+        // 풀 투사체 사용
+        projectile = projectilePool.Dequeue();
+
+        // 활성화
+        projectile.gameObject.SetActive(true);
+
+        return projectile;
     }
+    // 투사체 풀 반환
     public void ReturnToPool(Projectile projectile)
     {
-        // 투사체 풀 반환
+        projectilePool.Enqueue(projectile);
+        projectile.gameObject.SetActive(false);
+
+    }
+    #endregion
+
+    // 발사
+    public void Fire()
+    {
+        Projectile projectile = GetFromPool();
+
+        projectile.transform.position = transform.position;
+
+        Vector3 direction = (target.position - projectile.transform.position).normalized;
+
+        projectile.SetDirection(direction);
     }
 
-
-    public void Attack(Vector2 dir)
+    IEnumerator Attack()
     {
-        // 투사체 발사
-    }
+        while (true)
+        {
+            yield return fireState;
 
-    public void TickTime(float deltaTime)
-    {
-        // 공격 주기 갱신
+            Fire();
+
+            yield return attackInterval;
+        }
     }
 
     public void UpgradeWeapon()
