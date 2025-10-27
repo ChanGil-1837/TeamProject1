a5a374f3fbc5214b7774e3cb15b44d6326a289d4
chanho <chan.gil1837@gmail.com>
2025-10-27 12:06:43 +0900

    [feat] UI 스크롤 추가 및 디버그 기능 추가

--- Assets/02_Scripts/UI/UIManager.cs
+++ Assets/02_Scripts/UI/UIManager.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Net.Mime;
 using AYellowpaper.SerializedCollections;
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 namespace UI
 {
     public enum UIContentsType
     {
-        WEAPON, HEALTH, UTIL, NONE,
+        WEAPON,
+        HEALTH,
+        UTIL,
+        NONE,
     }
 
+    [ExecuteAlways] // 에디터 모드에서도 실행 가능
     public class UIManager : MonoBehaviour
     {
         [SerializeField] private Button weaponBtn;
@@ -20,19 +27,17 @@ namespace UI
         [SerializeField] private Button utilBtn;
         [SerializeField] private Button tempBtn;
         [SerializeField] private Button closeBtn;
-        // Start is called before the first frame update
 
         [SerializeField] private GameObject contentPanel;
-
         [SerializeField] private SerializedDictionary<UIContentsType, GameObject> uiContentsDic;
 
+        [SerializeField] private bool isPanelOpen = false;
         private UIContentsType contentType = UIContentsType.NONE;
 
-        [SerializeField] private bool isPanelOpen = false;
+        private const float CLOSEDSIZE = 80f;
+        private const float OPENSIZE = 720f;
+        private const float ANIMDURATION = 0.35f;
 
-        const float CLOSEDSIZE = 80f;
-        const float OPENSIZE = 720f;
-        const float ANIMDURATION = 0.35f;
         public UIContentsType ContentType
         {
             get => contentType;
@@ -43,6 +48,7 @@ namespace UI
                     isPanelOpen = true;
                     OpenPanel();
                 }
+
                 if (contentType != value)
                 {
                     ChangeContent(value);
@@ -51,76 +57,111 @@ namespace UI
             }
         }
 
-        void Start()
+        private void Start()
         {
-            weaponBtn.onClick.AddListener(() =>
+#if UNITY_EDITOR
+            if (!Application.isPlaying)
             {
-                ContentType = UIContentsType.WEAPON;
-            });
-            healthBtn.onClick.AddListener(() =>
-            {
-                ContentType = UIContentsType.HEALTH;
-            });
-            utilBtn.onClick.AddListener(() =>
-            {
-                ContentType = UIContentsType.UTIL;
-            });
+                return;
+            }
+#endif
+            weaponBtn.onClick.AddListener(() => { ContentType = UIContentsType.WEAPON; });
+            healthBtn.onClick.AddListener(() => { ContentType = UIContentsType.HEALTH; });
+            utilBtn.onClick.AddListener(() => { ContentType = UIContentsType.UTIL; });
+
             closeBtn.onClick.AddListener(() =>
             {
-                if (!isPanelOpen)
+                isPanelOpen = !isPanelOpen;
+
+                if (isPanelOpen)
                 {
-                    isPanelOpen = true;
                     OpenPanel();
                 }
                 else
                 {
-                    isPanelOpen = false;
                     ClosePanel();
                 }
             });
         }
 
-        // Update is called once per frame
-        void Update()
+        private void ChangeContent(UIContentsType type)
         {
-
-        }
-
-        void ChangeContent(UIContentsType type)
-        {
-            // 모든 콘텐츠 비활성화
             foreach (var kvp in uiContentsDic)
             {
                 if (kvp.Value != null)
+                {
                     kvp.Value.SetActive(false);
+                }
             }
 
-            // 선택한 타입의 콘텐츠만 활성화
             if (uiContentsDic.ContainsKey(type) && uiContentsDic[type] != null)
             {
                 uiContentsDic[type].SetActive(true);
             }
         }
-        void OpenPanel()
+
+        private void OpenPanel()
         {
-            RectTransform rect = contentPanel.GetComponent<RectTransform>();
-            if (rect != null)
+            RectTransform rect = contentPanel?.GetComponent<RectTransform>();
+            if (rect == null)
             {
-                // DOTween으로 높이만 변경
+                return;
+            }
+
+            if (Application.isPlaying)
+            {
                 rect.DOSizeDelta(new Vector2(rect.sizeDelta.x, OPENSIZE), ANIMDURATION)
                     .SetEase(Ease.OutCubic);
             }
+            else
+            {
+                rect.sizeDelta = new Vector2(rect.sizeDelta.x, OPENSIZE);
+            }
         }
 
-        void ClosePanel()
+        private void ClosePanel()
         {
-            RectTransform rect = contentPanel.GetComponent<RectTransform>();
-            if (rect != null)
+            RectTransform rect = contentPanel?.GetComponent<RectTransform>();
+            if (rect == null)
+            {
+                return;
+            }
+
+            if (Application.isPlaying)
             {
                 rect.DOSizeDelta(new Vector2(rect.sizeDelta.x, CLOSEDSIZE), ANIMDURATION)
                     .SetEase(Ease.InCubic);
             }
+            else
+            {
+                rect.sizeDelta = new Vector2(rect.sizeDelta.x, CLOSEDSIZE);
+            }
         }
-    }
 
+#if UNITY_EDITOR
+        private void OnValidate()
+        {
+            if (contentPanel == null)
+            {
+                return;
+            }
+
+            // 인스펙터에서 isPanelOpen 토글 시 즉시 반영
+            if (isPanelOpen)
+            {
+                OpenPanel();
+            }
+            else
+            {
+                ClosePanel();
+            }
+
+            // 에디터 뷰 갱신
+            if (!Application.isPlaying)
+            {
+                EditorApplication.QueuePlayerLoopUpdate();
+            }
+        }
+#endif
+    }
 }
