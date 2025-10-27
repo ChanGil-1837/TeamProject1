d43daa98866eb82ffebfd1784403a1b75b0045df
Hyun <oiwer23@gmail.com>
2025-10-28 01:58:17 +0900

    [feat] Player 스탯 업그레이드, 이벤트 구조 추가

--- Assets/02_Scripts/Player/Player.cs
+++ Assets/02_Scripts/Player/Player.cs
@@ -3,56 +3,95 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
+public enum UpgradeType
+{
+    MaxHp,
+    Defense,
+    HpRegen,
+    DetectRange,
+    GoldMultiplier,
+    Weapon
+}
 
 public class Player : MonoBehaviour
 {
-    [Header("Base Stats")]
-    [SerializeField] private float _baseHp = 100;//체력
-    [SerializeField] private float _baseMaxHp = 100;//최대체력
+    [Header("Ingame Stats")]
+    [SerializeField] private float _baseMaxHp = 100f;//최대체력
     [SerializeField] private float _baseHpRegen = 0.1f; //기본체력리젠
     [SerializeField] private float _baseDef = 0f; //방어력
-    [SerializeField] private int _gold;//보유 골드
     [SerializeField] private float _baseGoldMultiplier = 0f; //골드획득비율
+    [SerializeField] private float _detectRange = 5f;//적 탐색 범위
 
+    [Header("Player Gold")]
+    [SerializeField] private int _gold;//보유 골드
 
-
-    [SerializeField] private float _detectRange = 5;//적 탐색 범위
-    [SerializeField] private Weapon _weapon;//무기 넣을것
-    private float _currentHp;//변동된 현재 체력
+    [Header("Runtime State")]
+    private float _currentHp;//변동된 체력
     private bool _isDead = false;//사망여부
-    private readonly Dictionary<UpgradeType, float> _upgradeValue = new()//딕셔너리로 상승폭 관리
+
+    [Header("Weapon")]
+    [SerializeField] private Weapon _weapon;//무기 넣을것
+
+    //딕셔너리로 비용, 능력치 상승 관리
+    [Header("Upgrade Data")]
+    private readonly Dictionary<UpgradeType, (int cost, float value)> _upgradeValue = new Dictionary<UpgradeType, (int cost, float value)>
     {
-        {UpgradeType.MaxHp, 10f },
-        {UpgradeType.HpRegen, 0.1f },
-        {UpgradeType.DetectRange, 0.5f },
-        {UpgradeType.Defense, 0.5f },
-        {UpgradeType.GoldMultiplier, 0.05f }
+        {UpgradeType.MaxHp, (100, 10f) }, //비용, 상승치
+        {UpgradeType.HpRegen, (120, 0.1f) },
+        {UpgradeType.DetectRange, (200, 0.5f) },
+        {UpgradeType.Defense, (150, 0.5f) },
+        {UpgradeType.GoldMultiplier, (500, 0.05f) }
     };
+    private const float CostMultiplier = 1.2f; //비용 비율 20퍼
+    private const float RegenInterval = 1f; //1초마다 회복
+    private float _regenTimer = 0f;//누적용 타이머
+
+
     //읽기전용 프로퍼티 아직은 어떻게 돌아갈지 몰라서 대충 지정해둠
-    public float Hp => _baseHp;
+    public float CurrentHp => _currentHp;//변동된 체력
     public float MaxHp => _baseMaxHp;
     public float AttackRange => _detectRange;
     public float HpRegen => _baseHpRegen;
     public float Defense => _baseDef;
     public int Gold => _gold;
     public float GoldMultiplier => _baseGoldMultiplier;
+    public bool IsDead => _isDead;
 
-    public event Action OnStatsChanged;
-    public event Action<int> OnGoldChanged;
+
+    public event Action OnStatsChanged; //스텟 수치 변화시 호출
+    public event Action<int> OnGoldChanged; //골드 변화시 호출
+
     private void Start()
     {
-        _currentHp = _baseMaxHp;
-        _isDead = false;
+        Init();
     }
     private void Update()
     {
-        if (GameManager.Instance.IsPlaying() == false)
+        if (_isDead == true || GameManager.Instance.IsPlaying() == false)//정지 혹은 사망이라면
         {
             return;
         }
+        _regenTimer += Time.deltaTime; //프레임당 시간 누적
+
+        if (_regenTimer >= RegenInterval) //누적시간 >= 1초
+        {
+            TickTime(RegenInterval); //틱타임 1초간격
+            _regenTimer = 0f; //다시 초기화
+        }
     }
-    public void AddGold(int amount)
+    public void Init()
+    {
+        _baseMaxHp = 100f;
+        _currentHp = _baseMaxHp;
+        _detectRange = 5f;
+        _baseDef = 0f;
+        _baseGoldMultiplier = 0f;
+        _baseHpRegen = 0.1f;
+        _isDead = false;
+        _regenTimer = 0f;
+        OnStatsChanged?.Invoke();
+    }
+    public void AddGold(int amount)//골드 획득부분은 적리워드 필요(이야기할 필요가 있음),게임매니저의 개입이 낫다 생각됨(적죽음관련)
     {
         int finalGold = Mathf.RoundToInt(amount * (1 + _baseGoldMultiplier));
         _gold += finalGold;
@@ -68,19 +107,23 @@ public class Player : MonoBehaviour
         OnGoldChanged?.Invoke(_gold);
         return true;
     }
-    private void AddWeapon(Weapon weapon)
+    public void Attack(List<IEnemy> enemies)//가까운적 찾아서 공격
     {
+        if (_weapon == null)
+        {
+            return;
+        }
+        IEnemy target = FindClosestEnemy(enemies);//가까운 적의 위치를 찾고
+        if (target != null)//타겟이 있다면
+        {
+            //무기가 위치정보 받을 메서드 필요(이야기할 필요가 있음)
+            _weapon.Fire();
+        }
 
     }
-
-    private void Attack(List<IEnemy> enemies)//가까운적 찾아서 공격
-    {
-        IEnemy target = FindClosestEnemy(enemies);
-        //~~~무기의 공격을 호출
-    }
-
     public void TakeDamage(float damage)
     {
+        damage = Mathf.Max(damage - _baseDef, 1f);//방어력이 공격력보다 높다면 무적상태를 줄것인가?
         _currentHp -= damage;
         OnStatsChanged?.Invoke();
         if (_currentHp <= 0)
@@ -89,26 +132,19 @@ public class Player : MonoBehaviour
             GameManager.Instance.GameOver();
         }
     }
-
-    private void TickTime(float deltaTime)
+    public void UpgradeStats(UpgradeType type)
     {
-
-    }
-
-    private void UpgradeStats(UpgradeType type)
-    {
-        int cost = GetUpgradeCost(type);
-
+        if (_upgradeValue.TryGetValue(type, out (int cost, float value) data) == false)//딕셔너리에 해당 업그레이드 타입 정보가 없다면
+        {
+            return;
+        }
+        int cost = data.cost;
+        float value = data.value;
         if (SpendGold(cost) == false)//골드가 부족하면
         {
             return;
         }
-        if (_upgradeValue.ContainsKey(type) == false)//딕셔너리에 타입이 없다면
-        {
-            return;
-        }
-        float value = _upgradeValue[type]; //딕셔너리에 저장된 상승폭
-        switch (type)
+        switch (type)//효과 적용
         {
             case UpgradeType.MaxHp:
                 MaxHpUp(value);
@@ -132,32 +168,29 @@ public class Player : MonoBehaviour
                 }
                 break;
         }
+        int newCost = Mathf.RoundToInt(cost * CostMultiplier); //상승된 비용 적용
+        _upgradeValue[type] = (newCost, value); //딕셔너리에 반영
         OnStatsChanged?.Invoke();
     }
-    private int GetUpgradeCost(UpgradeType type)//업그레이드 비용 관련
+    public void Reset()//상점을 만들어서 기본능력치를 더 올릴것이라고 한다면 추가할 예정,아니라고 하면 삭제
     {
-        switch (type)
-        {
-            case UpgradeType.MaxHp:
-                return 100;
-            case UpgradeType.Defense:
-                return 150;
-            case UpgradeType.HpRegen:
-                return 120;
-            case UpgradeType.DetectRange:
-                return 200;
-            case UpgradeType.GoldMultiplier:
-                return 300;
-            case UpgradeType.Weapon:
-                return 500;
-            default:
-                return 0;
-        }
+        Init();//현재 동작은 Init과 같음
     }
+    private void AddWeapon(Weapon weapon)
+    {
+        //무기 장착 해야됨...................................................
+    }
+
+    private void TickTime(float deltaTime)
+    {
+        _currentHp = Mathf.Min(_currentHp + _baseHpRegen * deltaTime, _baseMaxHp);
+        OnStatsChanged?.Invoke();
+    }
+  
     private IEnemy FindClosestEnemy(List<IEnemy> enemies)//가까운 적 찾기
     {
-        IEnemy ClosestEnemy = null;
-        float ClosestDistance = float.MaxValue;
+        IEnemy closestEnemy = null;
+        float closestDistance = float.MaxValue;
         foreach (IEnemy enemy in enemies)
         {
             if (enemy == null || enemy.IsDead)
@@ -165,13 +198,13 @@ public class Player : MonoBehaviour
                 continue;
             }
             float distance = Vector3.Distance(transform.position, enemy.Transform.position);
-            if (distance < _detectRange && distance < ClosestDistance)
+            if (distance < _detectRange && distance < closestDistance)
             {
-                ClosestEnemy = enemy;
-                ClosestDistance = distance;
+                closestEnemy = enemy;
+                closestDistance = distance;
             }
         }
-        return ClosestEnemy;
+        return closestEnemy;
     }
     private void Die()
     {
@@ -184,8 +217,9 @@ public class Player : MonoBehaviour
         _baseHpRegen += value;
     }
     private void MaxHpUp(float value)
-    {
-        _baseMaxHp += value;
+    { 
+        _baseMaxHp += value; //최대 체력 상승
+        _currentHp = Mathf.Min(_currentHp, _baseMaxHp);//현재 체력은 변동 없도록
     }
     private void DefUp(float value)
     {
@@ -199,15 +233,6 @@ public class Player : MonoBehaviour
     {
         _baseGoldMultiplier += value;
     }
-
-    public enum UpgradeType
-    {
-        MaxHp,
-        Defense,
-        HpRegen,
-        DetectRange,
-        GoldMultiplier,
-        Weapon
-
-    }
+ 
+    
 }
